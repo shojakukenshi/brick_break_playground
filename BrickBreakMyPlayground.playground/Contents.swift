ccb4e76dc95b93f9657f9dbe58eb777e08387e99
Kenshi Shojaku <shojaku@gmail.com>
2018-07-13 09:50:00 +0900

    implement contact delegate

--- BrickBreakMyPlayground.playground/Contents.swift
+++ BrickBreakMyPlayground.playground/Contents.swift
@@ -3,11 +3,16 @@
 import PlaygroundSupport
 import SpriteKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     private var label : SKLabelNode!
     private var ball : SKShapeNode!
     private var block : SKShapeNode!
+    private var paddle : SKShapeNode!
+    
+    let categoryBall:UInt32 = 1 << 1 //0001
+    let categoryBlock:UInt32 = 1 << 2 //0010
+    let categoryPaddle:UInt32 = 1 << 3 //0100
     
     override func didMove(to view: SKView) {
         // Get label node from scene and store it for use later
@@ -17,61 +22,81 @@ class GameScene: SKScene {
                                            .fadeOut(withDuration: 2.0)])
         label.run(.repeatForever(fadeInOut))
         
-        // Create shape node to use during mouse interaction
+        // Create ball
         let w = (size.width + size.height) * 0.02
         
-        ball = SKShapeNode(circleOfRadius: w / 1.0)
+        ball = SKShapeNode(circleOfRadius: w / 2.0)
         ball.fillColor = SKColor.white
-        ball.physicsBody = SKPhysicsBody(circleOfRadius: w / 1.0)
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: w / 2.0)
         ball.physicsBody?.affectedByGravity = false
+        ball.physicsBody?.friction = 0.0
+        ball.physicsBody?.restitution = 1.0
+        ball.physicsBody?.linearDamping = 0.0
         ball.physicsBody?.velocity = CGVector(dx: 200.0, dy: 200.0)
+        
+        ball.physicsBody?.categoryBitMask = categoryBall
+        ball.physicsBody?.contactTestBitMask = categoryBlock | categoryPaddle
+        
 //        ball.physicsBody?.applyImpulse(CGVector(dx: 0.0, dy: 200.0))
         addChild(ball)
         
-        block = SKShapeNode(rectOf: CGSize(width: w, height: w))
+        // Create blocks
+        block = SKShapeNode(rectOf: CGSize(width: w * 2, height: w))
         block.fillColor = SKColor.gray
         block.physicsBody = SKPhysicsBody(rectangleOf: CGSize(
-                width: w,
+                width: w * 2.0,
                 height: w
             )
         )
         block.physicsBody?.affectedByGravity = false
-        for i in 1...10 {
-            for j in 1...10 {
-                if i==1 || i==10 || j==1 || j==10 {
-                    let b = block.copy() as! SKShapeNode
-                    b.position = CGPoint(
-                        x: self.frame.midX + CGFloat(j - 5) * w,
-                        y: self.frame.midY + CGFloat(i - 5) * w)
-                    addChild(b)
-                }
+        block.physicsBody?.categoryBitMask = categoryBlock
+        block.physicsBody?.contactTestBitMask = categoryBall
+        for i in 0...4 {
+            for j in 0...10 {
+                let b = block.copy() as! SKShapeNode
+                b.position = CGPoint(
+                    x: self.frame.midX + CGFloat(j - 5) * w * 2,
+                    y: self.frame.midY + size.height / 3.0 + CGFloat(i - 5) * w)
+                addChild(b)
             }
         }
         
+        // Create paddle
+        paddle = SKShapeNode(rectOf: CGSize(width: w * 4, height: w), cornerRadius: w * 0.2)
+        paddle.fillColor = SKColor.red
+        paddle.position = CGPoint(
+            x: self.frame.midX,
+            y: self.frame.midY - size.height / 3.0
+        )
+        paddle.physicsBody = SKPhysicsBody(rectangleOf: CGSize(
+                width: w * 4,
+                height: w
+            )
+        )
+        paddle.physicsBody?.affectedByGravity = false
+        paddle.physicsBody?.mass = 99999999.9
+        paddle.physicsBody?.categoryBitMask = categoryPaddle
+        addChild(paddle)
+        
+        self.physicsWorld.contactDelegate = self
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        let blockBody = contact.bodyA
+        blockBody.affectedByGravity = true
     }
     
     func touchDown(atPoint pos : CGPoint) {
-        guard let n = ball.copy() as? SKShapeNode else { return }
-        
-        n.position = pos
-        n.strokeColor = SKColor.green
-        addChild(n)
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        guard let n = self.ball.copy() as? SKShapeNode else { return }
-        
-        n.position = pos
-        n.strokeColor = SKColor.blue
-        addChild(n)
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        guard let n = ball.copy() as? SKShapeNode else { return }
-        
-        n.position = pos
-        n.strokeColor = SKColor.red
-        addChild(n)
+        paddle.position = CGPoint(
+            x: (paddle.position.x + pos.x) / 2.0,
+            y: paddle.position.y
+        )
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
